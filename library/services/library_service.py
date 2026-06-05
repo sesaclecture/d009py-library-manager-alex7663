981bac73414cb8d9bdf0640004a139b73a1d3949
alex <alexander7663@gmail.com>
2026-06-05 19:33:06 +0900

    파이썬 마지막 과제 제출합니다.

--- library/services/library_service.py
+++ library/services/library_service.py
@@ -1,4 +1,3 @@
-
 from __future__ import annotations
 from typing import Iterable, List
 from library.models.book import Book
@@ -14,21 +13,28 @@ class LibraryService(BaseService):
     """
 
     def __init__(self) -> None:
-        # TODO: 내부 리스트 초기화
-        pass
+        # TOD: 내부 리스트 초기화
+        self._books = []
 
     def add_book(self, book: Book) -> None:
-        # TODO: 책 추가
-        raise NotImplementedError
+        # TOD: 책 추가
+        self._books.append(book)
 
     def remove_book(self, title: str) -> None:
-        # TODO: 제목으로 책 삭제 (없으면 ValueError)
-        raise NotImplementedError
+        # TOD: 제목으로 책 삭제 (없으면 ValueError)
+        for book_remove in self._books:
+            if book_remove.title == title:
+                self._books.remove(book_remove)
+                return
+        raise ValueError
 
     def list_books(self) -> Iterable[Book]:
-        # TODO: 책 목록 반환 (복사본 반환 권장)
-        raise NotImplementedError
+        # TOD: 책 목록 반환 (복사본 반환 권장)
+        return self._books.copy()
 
     def find_book(self, title: str) -> Book:
-        # TODO: 제목으로 책 찾기 (없으면 ValueError)
-        raise NotImplementedError
+        # TOD: 제목으로 책 찾기 (없으면 ValueError)
+        for book_find in self._books:
+            if book_find.title == title:
+                return book_find
+        raise ValueError
